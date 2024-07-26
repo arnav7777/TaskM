243e84792a0d6cd6c7c6062abbe9ff4027bb3895
arnav7777 <arnav.ps@somaiya.edu>
2024-07-26 13:17:25 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/TaskM",
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'src/main.jsx', // Adjust if necessary
+    },
   }
 })
